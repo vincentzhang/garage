320af18629f099b80da0f24a403f65dc3a91f869
Zhanpeng He <zhanpenh@usc.edu>
2019-01-21 17:08:31 -0800

    Meta training done, need a lot of cleanup

--- garage/envs/point_env.py
+++ garage/envs/point_env.py
@@ -6,7 +6,12 @@ from garage.envs import Step
 
 
 class PointEnv(gym.Env, Serializable):
-    def __init__(self):
+    def __init__(self, goal=None):
+        if goal is None:
+            self.goal = np.array([0, 0])
+        else:
+            self.goal = goal
+
         # Always call Serializable constructor last
         Serializable.quick_init(self, locals())
 
@@ -26,15 +31,26 @@ class PointEnv(gym.Env, Serializable):
         return observation
 
     def step(self, action):
-        self._state = self._state + action
+        # a = action.copy()
+        # a = np.clip(a, self.action_space.low, self.action_space.high)
+        self._state = self._state + np.clip(action, self.action_space.low, self.action_space.high)
         x, y = self._state
-        reward = -(x**2 + y**2)**0.5
-        done = abs(x) < 0.01 and abs(y) < 0.01
+        reward = -((x - self.goal[0])**2 + (y - self.goal[1])**2)**0.5
+        done = False
         next_observation = np.copy(self._state)
+        # print(reward, self.goal)
         return Step(observation=next_observation, reward=reward, done=done)
 
-    def render(self):
+    def render(self, mode=None):
         print('current state:', self._state)
 
     def log_diagnostics(self, paths):
         pass
+
+    def reset_task(self, task):
+        self.goal = task
+
+    @property
+    def task_space(self):
+        return gym.spaces.Box(
+            low=-10., high=10., shape=(2, ), dtype=np.float32)
